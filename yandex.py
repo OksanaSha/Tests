3a015d4e16e0b335c4695ba6f27fbe70a32d5881
OksanaSha <oksanashatalova@bk.ru>
2021-10-08 22:02:45 +0300

    def upload_file

--- yandex.py
+++ yandex.py
@@ -13,10 +13,20 @@ class YandexDisk:
             'Authorization': f'OAuth {self.token}'
         }
 
-    def _get_upload_link(self, path_to_file):
+    def _get_upload_link(self, disk_file_path):
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        params = {'path': path_to_file}
+        params = {'path': disk_file_path, 'overwrite': 'true'}
         response = requests.get(url=upload_url, params=params, headers=headers)
-        pprint(response.json())
         return response.json()
+
+    def upload_file(self, disk_file_path, file):
+        response = self._get_upload_link(disk_file_path=disk_file_path)
+        url = response.get('href', '')
+        if url:
+            response = requests.put(url=url, data=open(file, 'rb'))
+            response.raise_for_status()
+            if response.status_code == 201:
+                print('Success')
+        else:
+            print('Empty url')
