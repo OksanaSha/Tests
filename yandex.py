5c6464f0fbab6c778acf524251d27587f63b2609
OksanaSha <oksanashatalova@bk.ru>
2021-10-08 21:03:55 +0300

    class YandexDisk _get_upload_link()

---
+++ yandex.py
@@ -0,0 +1,20 @@
+import requests
+
+
+class YandexDisk:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def _get_upload_link(self, path_to_file):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': '/Users/oks/Desktop/toni_stark.jpg'}
+        response = requests.get(url=upload_url, params=params, headers=headers)
+        pprint(response.json())
