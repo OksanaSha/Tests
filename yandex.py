8b3506186a445e98b696cb344f275f00a0999531
OksanaSha <oksanashatalova@bk.ru>
2021-10-08 21:26:26 +0300

    jpg

--- yandex.py
+++ yandex.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 
 
 class YandexDisk:
@@ -15,6 +16,7 @@ class YandexDisk:
     def _get_upload_link(self, path_to_file):
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        params = {'path': '/Users/oks/Desktop/toni_stark.jpg'}
+        params = {'path': path_to_file}
         response = requests.get(url=upload_url, params=params, headers=headers)
         pprint(response.json())
+        return response.json()
